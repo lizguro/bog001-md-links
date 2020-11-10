2c44fe1b1d08e662381396f25f60bdfef4f60727
lizguro <dianyguti@gmail.com>
2020-11-10 12:04:21 -0500

    readme

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-module.exports = () => {
-  // ...
-};
+const functions = require('./lib/functions');
+const validateLinks = require('./lib/validate');
+
+const { resolve } = require('path');
+const path = require('path');
+const fs = require('fs');
+
+// *---Validar si ruta existe y es absoluta----
+const validateRoute = ((pathUser) => {
+  if (fs.existsSync(pathUser)) {
+    if (!path.isAbsolute(pathUser)) {
+      const pathResolved = resolve(pathUser);
+      return pathResolved;
+    }
+  } 
+});
+
+//**--Función principal MDLINKS----- */
+const mdLinks = (pathUser,options={})=> {
+  return new Promise((resolve, reject) => {
+    const validPath = validateRoute(pathUser);
+    functions.getLinks(validPath)
+    .then((links)=>{
+      if (options.validate) {
+        resolve(validateLinks(links));
+      }
+      else {
+       console.log("respueta de getlinks");
+       console.log(links);
+        resolve(links);
+       }
+     
+    })
+    .catch((err)=>{
+      console.log(err.message);
+      reject(err)
+    });
+    
+  });
+}
+
+module.exports = mdLinks;
+module.exports = validateRoute;
